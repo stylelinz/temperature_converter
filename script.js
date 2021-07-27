8c1537e08e995684067d7e2a50c6979c908b91ce
stylelinz <cwchuang1997@gmail.com>
2021-07-27 20:32:31 +0800

    refactor: convert function

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const celsiusInput = document.querySelector('#Celsius')
-const fahrenheitInput = document.querySelector('#Fahrenheit')
 const temperatures = document.querySelectorAll('input[type=number]')
 const converterForm = document.querySelector('form')
 
-const degreeC = 0
+const temperatureElements = {}
 
 temperatures.forEach(temperatureInput => {
+  temperatureElements[temperatureInput.id] = temperatureInput
   temperatureInput.addEventListener('keydown', validInput)
 })
 
+const convertFunctions = {
+  Celsius () {
+    temperatureElements.Fahrenheit.value = (temperatureElements.Celsius.value * 1.8 + 32).toFixed(2)
+  },
+  Fahrenheit () {
+    temperatureElements.Celsius.value = ((temperatureElements.Fahrenheit.value - 32) / 1.8).toFixed(2)
+  }
+}
+
 converterForm.addEventListener('input', (event) => {
   const { id } = event.target
-  if (id === 'Celsius') {
-    fahrenheitInput.value = (celsiusInput.value * 1.8 + 32).toFixed(2)
-  }
-  if (id === 'Fahrenheit') {
-    celsiusInput.value = ((fahrenheitInput.value - 32) / 1.8).toFixed(2)
-  }
+  convertFunctions[id]()
 })
 
 function validInput (event) {
   const { keyCode } = event
-  // backspace, negative sign, digit
-  const specialKeyCode = [8, 173, 190]
+  // backspace, left & right, negative sign, digit
+  const specialKeyCode = [8, 37, 39, 173, 190]
   if (specialKeyCode.indexOf(keyCode) !== -1) return
   if (keyCode >= 48 && keyCode <= 57) return
   if (keyCode >= 96 && keyCode <= 105) return
